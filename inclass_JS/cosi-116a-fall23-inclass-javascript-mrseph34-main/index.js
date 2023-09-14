4dd5d79e9d792d696584804a5551202481c2e2f2
Joseph Donaldson <41586191+mrseph34@users.noreply.github.com>
2023-09-14 14:08:42 -0400

    added fancy stuff ig
    buttons cool wow nice!

--- inclass_JS/cosi-116a-fall23-inclass-javascript-mrseph34-main/index.js
+++ inclass_JS/cosi-116a-fall23-inclass-javascript-mrseph34-main/index.js
@@ -2,12 +2,26 @@ function multiplyBy()
 {
         num1 = document.getElementById("firstNumber").value;
         num2 = document.getElementById("secondNumber").value;
-        document.getElementById("result").innerHTML = num1 * num2;
+        document.getElementById("result").innerHTML = (parseInt(num1) * parseInt(num2));
 }
 
 function divideBy() 
 { 
         num1 = document.getElementById("firstNumber").value;
         num2 = document.getElementById("secondNumber").value;
-document.getElementById("result").innerHTML = num1 / num2;
+document.getElementById("result").innerHTML = (parseInt(num1) / parseInt(num2));
+}
+
+function subBy() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = (parseInt(num1) - parseInt(num2));
+}
+
+function addBy() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = (parseInt(num1) + parseInt(num2));
 }
